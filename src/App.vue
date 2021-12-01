e2f9a87b1a7ebd6836cf8e2615cb0abaf27ac6ae
1AlbertChen <zhenrongchen2021@u.northwestern.edu>
2021-12-01 17:45:47 +0800

    made pool

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
   <Renderer
     ref="renderer"
     antialias
-    :orbit-ctrl="{enableZoom: true}"
+    :orbit-ctrl="{enableDamping: true}"
     resize="window"
     :pointer="{ onMove: onPointerMove }"
     shadow>
-    <Camera ref="camera" :position="{x: 100, y: 70, z: 30}" :lookAt="{ x: 0, y: 0, z: 0}" :far="30000"/>
-    <Scene ref="scene">
+    <Camera ref="camera" :position="{x: 20, y: 30, z: 0}"/>
+    <Scene ref="scene" :background="cubeTexture">
       <PointLight
         ref="mouse"
         :intensity="0.1"
@@ -19,20 +19,15 @@
       </PointLight>
       <PointLight
         ref="main"
-        :position="{ y: 100, z: 50 }"
+        :position="{ y: 40 }"
         :intensity="brightness"
         cast-shadow>
       </PointLight>
       <AmbientLight />
-      <PointLight :color="light1Color" :position="{ x: 50, y: 50, z: 50}" />
-      <PointLight :color="light2Color" :position="{ x: -50, y: 50, z: 50}" />
-      <PointLight :color="light3Color" :position="{ x: -50, y: -50, z: 50}" />
-      <PointLight :color="light4Color" :position="{ x: 50, y: -50, z: 50}" />
-
-      <FbxModel src="/assets/models/Dying.fbx"
+      <!-- <FbxModel src="/assets/models/Dying.fbx"
         @load="onLoad" 
         :position="{y: 5, z: -10}"
-        :scale="{ x: 0.1, y: 0.1, z: 0.1}"/>
+        :scale="{ x: 0.1, y: 0.1, z: 0.1}"/> -->
       <!-- <GltfModel src="/assets/models/pony_cartoon/scene.gltf" 
       :position="{y: 5, z: -10}"
       /> -->
@@ -56,7 +51,7 @@
           cast-shadow>
           <PhongMaterial color="#ffffff" />
         </Sphere>
-        <Box
+        <!-- <Box
           :width="100"
           :depth="100"
           :position="{x:0, y: -1, z: 0}"
@@ -68,18 +63,20 @@
             <Texture :props="texturesProps" src="/assets/textures/Wood_Tiles_002_roughness.jpg" name="roughnessMap" />
             <Texture :props="texturesProps" src="/assets/textures/Wood_Tiles_002_ambientOcclusion.jpg" name="aoMap" />
           </StandardMaterial>
-        </Box>
+        </Box> -->
         
       </CannonWorld>
+     
       <LiquidPlane
           ref="liquid"
           :width="WIDTH"
           :height="HEIGHT"
-          :width-segments="512"
-          :height-segments="512"
-          :metalness="metalness"
           :roughness="roughness"
-          :color="color"/>
+          :metalness="metalness"
+          :width-segments="512" 
+          :height-segments="512"
+          :rotation="{ x: -Math.PI / 2 }"/>
+     
     </Scene>
   </Renderer>
 </template>
@@ -95,27 +92,38 @@ export default {
     CannonWorld,
     LiquidPlane
   },
-  data(){
-    //ball
-    function coGen(n){
-      var coords = []
-      for (var i = 0; i <= n; i++){
-        for (var j = -i; j <= i; j+=2){
-          coords.push({x: j, y: 1, z: i * Math.sqrt(3)})
-        }
+  setup(){
+    //skybox
+    let imageArray = [
+    '/assets/skybox/tears_ft.jpg',
+    '/assets/skybox/tears_bk.jpg',
+    '/assets/skybox/tears_up.jpg',
+    '/assets/skybox/tears_dn.jpg',
+    '/assets/skybox/tears_rt.jpg',
+    '/assets/skybox/tears_lf.jpg',];
+
+    const cubeTexture = new THREE.CubeTextureLoader().load(imageArray)
+    
+    //balls
+    const RANKS = 13
+    let coords = []
+    for (let i = 0; i <= RANKS; i++){
+      for (let j = -i; j <= i; j+=2){
+        coords.push({x: j, y: 1, z: i * Math.sqrt(3)})
       }
-      return coords
     }
-    var coords = coGen(13);
-    var count = coords.length;
-    const colors = new Array(count).fill().map(() => niceColors[20][Math.floor(THREE.MathUtils.randFloat(0, 5))]);
-    var balls = [];
-    for (var i = 0; i < count; i++){
+    const colors = new Array(coords.length).fill().map(() => niceColors[20][Math.floor(THREE.MathUtils.randFloat(0, 5))]);
+    let balls = [];
+    for (var i = 0; i < coords.length; i++){
       balls.push({position: coords[i], color: colors[i]})
     }
-    //
     return{
-      balls: balls,
+      cubeTexture,
+      balls
+    }
+  },
+  data(){
+    return{
       isInit: true,
       texturesProps: {
         repeat: { x: 5, y: 5 },
@@ -123,18 +131,58 @@ export default {
         wrapT: THREE.RepeatWrapping,
       },
       brightness: 1,
-      metalness: 1,
+      color: 0xffffff,
+      metalness: 0.7,
       roughness: 0.2,
-      light1Color: '#FFFF80',
-      light2Color: '#DE3578',
-      light3Color: '#FF4040',
-      light4Color: '#0d25bb',
-      color: '#ffffff',
-      WIDTH: 30,
-      HEIGHT: 30,
+      WIDTH: 20,
+      HEIGHT: 20,
     }
   },
+  //load mng
+  beforeCreate() {
+    THREE.DefaultLoadingManager.onLoad =  () => {
+      console.log('Loading Complete!');
+      document.getElementById("loading-screen").remove();
+    }
+  },
+  mounted() {
+    //tweakpane
+    this.pane = new Pane();
+    this.pane.addInput(this, 'brightness', { min: 0, max: 3 });
+    this.pane.addInput(this, 'metalness', { min: 0, max: 1 });
+    this.pane.addInput(this, 'roughness', { min: 0, max: 1 });
+
+    //scene core
+    const renderer = this.$refs.renderer;
+    const scene = this.$refs.scene.scene;
+
+    //main light helper
+    const main = this.$refs.main.light;
+    const lightHelper = new THREE.PointLightHelper(main)
+    scene.add(lightHelper);
+
+    //liquid
+    this.liquidEffect = this.$refs.liquid.liquidEffect;
+    this.liquidEffect.addDrop(0, 0, 0.05, 0.05);
+
+    this.raycaster = new THREE.Raycaster();
+    this.pointerPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
+    this.pointerV3 = new THREE.Vector3();
+    
+    //lantern
+    const mouse = this.$refs.mouse.light;
+    this.pointer = renderer.three.pointer
+    const mouseV3 = this.pointer.positionV3;
+
+    renderer.onBeforeRender(() => {
+      mouse.position.copy(mouseV3);
+    })
+  },
+  unmounted() {
+    this.pane.dispose();
+  },
   methods:{
+    //initialize cue and balls
     onBeforeStep(){
       if(this.isInit){
         this.cue.userData.body.velocity.set(0, 0, 10);
@@ -148,11 +196,13 @@ export default {
     initBalls(mesh){
       mesh.userData.mass = 1;
     },
+
+    //animation logic
     onLoad(object) {
       this.mixer = new THREE.AnimationMixer(object);
       const action = this.mixer.clipAction(object.animations[0]);
       action.play();
-      object.traverse(function (child) {
+      object.traverse((child) => {
         if (child.isMesh) {
           child.castShadow = true;
           child.receiveShadow = true;
@@ -164,79 +214,15 @@ export default {
     updateMixer() {
       this.mixer.update(this.clock.getDelta());
     },
-     onPointerMove() {
+
+    //liquid
+    onPointerMove() {
       this.raycaster.setFromCamera(this.pointer.positionN, this.$refs.renderer.three.camera);
       this.raycaster.ray.intersectPlane(this.pointerPlane, this.pointerV3);
       const x = 2 * this.pointerV3.x / this.WIDTH;
-      const y = 2 * this.pointerV3.y / this.HEIGHT;
+      const y = 2 * -this.pointerV3.z / this.HEIGHT;
       this.liquidEffect.addDrop(x, y, 0.025, 0.005);
     },
-  },
-  beforeCreate() {
-    THREE.DefaultLoadingManager.onLoad = function () {
-      console.log('Loading Complete!');
-      document.getElementById("loading-screen").remove();
-    }
-  },
-  mounted() {
-    //scene core
-    const renderer = this.$refs.renderer;
-    const scene = this.$refs.scene.scene;
-
-    //main light
-    const main = this.$refs.main.light;
-    const lightHelper = new THREE.PointLightHelper(main)
-    scene.add(lightHelper);
-
-    //orbit
-    const orbitCtrl = renderer.three.cameraCtrl;
-    orbitCtrl.target.set(0, 10, 0)
-
-    //skybox
-    let textureArray = [];
-    let materialArray = [];
-    let imageArray = [
-    '/assets/skybox/tears_ft.jpg',
-    '/assets/skybox/tears_bk.jpg',
-    '/assets/skybox/tears_up.jpg',
-    '/assets/skybox/tears_dn.jpg',
-    '/assets/skybox/tears_rt.jpg',
-    '/assets/skybox/tears_lf.jpg',];
-    imageArray.forEach(element => textureArray.push(new THREE.TextureLoader().load(element)))
-    textureArray.forEach(element => materialArray.push(new THREE.MeshBasicMaterial( { map: element })))
-    for (let i = 0; i < 6; i++)
-      materialArray[i].side = THREE.BackSide;
-    let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
-    let skybox = new THREE.Mesh( skyboxGeo, materialArray );
-    scene.add( skybox );
-
-    //tweakpane
-    this.pane = new Pane();
-    this.pane.addInput(this, 'brightness', { min: 0, max: 3 });
-    
-    //lantern
-    const mouse = this.$refs.mouse.light;
-    this.pointer = this.$refs.renderer.three.pointer
-    const mouseV3 = this.pointer.positionV3;
-
-    //liquid
-    this.liquidEffect = this.$refs.liquid.liquidEffect;
-    this.liquidEffect.addDrop(0, 0, 0.05, 0.05);
-
-    this.raycaster = new THREE.Raycaster();
-    this.pointerPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-    this.pointerV3 = new THREE.Vector3();
-
-
-
-    //animation loop
-
-    renderer.onBeforeRender(() => {
-      mouse.position.copy(mouseV3);
-    })
-  },
-  unmounted() {
-    this.pane.dispose();
   },
 }
 </script>
